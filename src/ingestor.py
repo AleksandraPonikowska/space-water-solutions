f0c4e5f56709953cccc1e59256ca63e09cf56a13
Ola <ponikowska.books@gmail.com>
2026-04-25 21:31:41 +0200

    Things

--- src/ingestor.py
+++ src/ingestor.py
@@ -70,20 +70,19 @@ def get_data(REGION_NAME):
           { id: "ndvi", bands: 1 },
           { id: "ndwi", bands: 1 },
           { id: "ndmi", bands: 1 },
-          { id: "dataMask", bands: 1 } // <--- TO JEST KLUCZOWE
+          { id: "dataMask", bands: 1 }
         ]
       };
     }
     function evaluatePixel(samples) {
-      // Ignorujemy chmury i cienie
-      if ([3, 8, 9, 10].includes(samples.SCL) || samples.dataMask === 0) {
+
+        if ([3, 8, 9, 10].includes(samples.SCL) || samples.dataMask === 0) {
           return { ndvi: [NaN], ndwi: [NaN], ndmi: [NaN], dataMask: [0] };
       }
       let ndvi = (samples.B08 - samples.B04) / (samples.B08 + samples.B04);
       let ndwi = (samples.B03 - samples.B08) / (samples.B03 + samples.B08);
       let ndmi = (samples.B08 - samples.B11) / (samples.B08 + samples.B11);
       
-      // Zwracamy wszystkie parametry + potwierdzenie, że piksel jest prawidłowy (1)
       return { ndvi: [ndvi], ndwi: [ndwi], ndmi: [ndmi], dataMask: [1] };
     }
     """
@@ -98,7 +97,7 @@ def get_data(REGION_NAME):
             },
             "aggregation": {
                 "timeRange": {"from": "2025-06-01T00:00:00Z", "to": "2026-04-20T00:00:00Z"},
-                "aggregationInterval": {"of": "P5D"},
+                "aggregationInterval": {"of": "P1M"},
                 "evalscript": evalscript,
                 "resx": 0.0005, "resy": 0.0005
             }
